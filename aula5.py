b49702c48d26af4c3eb160322735ec2288ec94b9
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-01-13 18:01:20 -0300

    att

---
+++ aula5.py
@@ -0,0 +1,27 @@
+# OPERADORES DE COMPARAÇÃO
+
+'''
+
+a = 'Vinicius'
+b = 'vinicius'
+
+print(a == b) # A resposta sera em Boolean(False ou True)
+print(ord('a'))
+print(ord('A'))
+
+'''
+
+# UTILIZANDO IF, ELIF, ELSE
+
+'''
+
+idade = int(input('Digite sua idade: '))
+
+if idade < 18:
+    print('Menor de idade')
+elif 18 >= idade < 60:
+    print('Maior de idade')
+else:
+    print('Idoso')
+    
+'''
